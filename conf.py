3e7bf994a603963ffbdc79740a59c4c90b0edf80
Vitaliy Pavlenko <vivpavlenko@gmail.com>
2016-11-14 23:26:08 +0200

    Nikola auto commit.
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -893,7 +893,7 @@ CONTENT_FOOTER_FORMATS = {
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "?"
+COMMENT_SYSTEM_ID = "https-pavlenkov-github-io-blog"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
